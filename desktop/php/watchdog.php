9dd173da4b16a8c9fc663f429debe9e4b2bbfa7c
MARY <56442712+TomsnCo@users.noreply.github.com>
2021-12-06 16:50:06 +0100

    Update

--- desktop/php/watchdog.php
+++ desktop/php/watchdog.php
@@ -107,8 +107,9 @@ foreach ($eqLogics as $eqLogic) {
                         <?php
 
 /* foreach (jeeObject::all() as $object) {
-	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>'; */
-}
+	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+} */
+			    
 /* Modifications apportées */
 foreach ((jeeObject::buildTree(null, false)) as $object) {
 	echo '<option value="' . $object->getId() . '">' . str_repeat('&nbsp;&nbsp;', $object->getConfiguration('parentNumber')) . $object->getName() . '</option>';
